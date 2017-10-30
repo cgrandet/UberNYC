a18d6c765383e2b77b29671387b92866b6886cab
Carlos Grandet <carlos.grandet@gmail.com>
2017-10-29 22:53:33 -0400

    Changes to folder

---
+++ concerts/csv_to_json.py
@@ -0,0 +1,29 @@
+### Computer Science with Applications III
+### Analyzing NYC Taxi Data
+### Lauren Dyson, Carlos Grandet, Hector Salvador
+### June 2016
+
+import csv
+import json
+from datetime import datetime
+
+def go():
+
+	f = open('sorted_bands.csv', 'r')
+	reader = csv.DictReader(f)
+	d = {}
+
+	for row in reader:
+		name = "{} @{}, {}".format(row['artist'], row['venue'], row['datetime'])
+		d[name] = {}
+		d[name]['pickup'] = (eval(row['lat']), eval(row['lon']))
+		d[name]['date'] = row['datetime']
+		year = datetime.strptime(row['datetime'], '%Y-%m-%dT%H:%M:%S').year
+
+	with open('sorted_bands.json'.format(year), 'w') as fp:
+		json.dump(d, fp)
+
+
+if __name__ == '__main__':
+    go()
+
